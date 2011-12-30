3d99bc43add6c886194529f4f4da1f5b79b6d680
Josep <apuratepp@gmail.com>
2011-12-30 13:55:52 +0100

    primer canvis

--- src/com/equip9/meteoub/MeteoUB.java
+++ src/com/equip9/meteoub/MeteoUB.java
@@ -2,6 +2,7 @@ package com.equip9.meteoub;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MeteoUB extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,7 @@ public class MeteoUB extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final TextView temperatura = (TextView) findViewById(R.id.temperatura);
+        temperatura.setText("suooola!");
     }
 }
