d310892e5293031de302d19706d6d3812430a057
Josep <apuratepp@gmail.com>
2012-01-01 15:38:14 +0100

    afegit background, refresh i info

--- src/com/equip9/meteoub/MeteoUB.java
+++ src/com/equip9/meteoub/MeteoUB.java
@@ -2,6 +2,8 @@ package com.equip9.meteoub;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,16 +11,18 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+/*
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
+*/
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+// import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class MeteoUB extends Activity {
@@ -28,10 +32,11 @@ public class MeteoUB extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         final TextView temperatura = (TextView) findViewById(R.id.temperatura);
-        temperatura.setText("suooola!");
+        temperatura.setText("À?¼C");
         refreshTemperatura();
     }
 	private void refreshTemperatura() {
+        
 		URL url;
 		try{
 			String meteoFeed = "http://ulisses.fis.ub.edu:8001/services/meteo/meteo.xml";
@@ -58,6 +63,8 @@ public class MeteoUB extends Activity {
 				final TextView temperatura = (TextView) findViewById(R.id.temperatura);
 				
 		        temperatura.setText(tempString+"¼C");
+		        final TextView comentari = (TextView) findViewById(R.id.comentari);
+		        comentari.setText("");
 			}
 		} catch(MalformedURLException e) {
 			e.printStackTrace();
@@ -71,4 +78,34 @@ public class MeteoUB extends Activity {
 			
 		}
 	}
+	
+	private void mostrarInfo() {
+		final TextView comentari = (TextView) findViewById(R.id.comentari);
+        comentari.setText("MŽs informaci—: apuratepp@gmail.com");
+	}
+	
+	static final private int MENU_REFRESH = Menu.FIRST;
+	static final private int MENU_INFO = Menu.FIRST+1;
+	
+	/* Creates the menu items */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU_REFRESH, 0, "Refrescar");
+        menu.add(0, MENU_INFO, 0, "Info");
+        return true;
+    }
+ 
+    /* Handles item selections */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+    	case MENU_REFRESH:
+    		final TextView comentari = (TextView) findViewById(R.id.comentari);
+    		comentari.setText("Connectant amb MeteoUB...");
+    		refreshTemperatura();
+            return true;
+    	case MENU_INFO:
+    		mostrarInfo();
+    		return true;
+        }
+        return false;
+    }
 }
